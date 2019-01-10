4f39e443c2bda916e81ba585656382f1e3c6a925
hsmith876 <32525207+hsmith876@users.noreply.github.com>
2019-01-10 11:05:20 -0500

    Update autocomplete.py

--- autocomplete.py
+++ autocomplete.py
@@ -1,28 +1,28 @@
 def extract(query):
-    """extract takes in a `query` API function (which returns the first 5 usernames, lexicographically sorted,
-    that start with a prefix) and returns the sorted list of all usernames in the database.
-
-    For example, the `query` function in provided in `main` works as follows:
-    
-    query("a") #=> ["abracadara", "al", "alice", "alicia", "allen"]
-    query("ab") #=> ["abracadara"]
-
-    The following implementation would pass the assertion in `main`, but is not a correct solution since it
-    works only for that example `query`:
-
-    def extract(query):
-        return query("ab") + query("al") + query("altercation") + query("b") + query("el") + query("ev") + query("m")
-
-    Your goal is to write an `extract` method that is correct for any provided `query`.
-    """
-    # YOUR CODE HERE
-    return [...]
-
+	q = "a"
+	wd = set()
+	def add1(c):
+		if c=='z':
+			return 0
+		return chr(ord(c)+1)
+	while q!='':
+		part=query(q)
+#		print(q)
+#		print(part)
+		wd =wd.union(set(part))
+		if len(part)==5:
+			q = q + part[-1][len(q)]
+		else:
+			next = add1(q[-1]) 
+			while q!='' and next==0:
+				q=q[:-1]
+				if q!='':
+					next = add1(q[-1]) 
+			if q!='':
+				q=q[:-1]+next
+	return sorted(list(wd))
 def main():
-    """Runs your solution -- no need to update (except to maybe try out different databases)."""
-    # Sample implementation of the autocomplete API
-    database = ["abracadara", "al", "alice", "alicia", "allen", "alter", "altercation", "bob", "element", "ello", "eve", "evening", "event", "eventually", "mallory"]
+    database = ["aabracadara","abracadara", "al", "alice", "alicia", "allen", "alter", "altercation", "bob", "element", "ello", "eve", "evening", "event", "eventually", "mallory"]
     query = lambda prefix: [d for d in database if d.startswith(prefix)][:5]
     assert extract(query) == database
-
 main()
